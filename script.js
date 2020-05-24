09e06650a60028fe24ec8cfc509abff1d4809797
Alberto Roura <mail@albertoroura.com>
2020-05-24 13:39:59 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -3,15 +3,10 @@ const outputArea = document.getElementById('supreme-area');
 function updateLogo () {
   const textInputValue = document.getElementById('text-input').value;
   const sizeSliderValue = document.getElementById('size-slider').value;
-  const outputStyle = 'font-size:' + sizeSliderValue + 'pt;' + 'padding-left:' + (sizeSliderValue / 30) + 'vh;' + 'padding-right:' + (sizeSliderValue / 30) + 'vh;' + 'padding-bottom:' + (sizeSliderValue / 40) + 'vh;';
+  const outputStyle = 'font-size:' + sizeSliderValue + 'pt;' + 'padding-left:' + (sizeSliderValue / 30) + 'vh;' + 'padding-right:' + (sizeSliderValue / 30) + 'vh;' + 'padding-bottom:' + (sizeSliderValue / 40) + 'vh';
 
   outputArea.setAttribute('style', outputStyle);
-
-  if (textInputValue !== '') {
-    outputArea.innerHTML = textInputValue;
-  } else {
-    outputArea.innerHTML = 'Supreme';
-  }
+  outputArea.innerHTML = textInputValue !== '' ? textInputValue : 'Supreme';
 }
 
 setInterval(updateLogo, 30);
